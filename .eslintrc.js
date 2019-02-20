b97c8028274e3e39960944a074dcaf7aafbf838d
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-20 09:54:16 -0800

    Initialize nuxt project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs'
+  ],
+  // add your custom rules here
+  rules: {}
+}
