4ac1a92adc4b45a340b1fbf6dc27bcb5642cd5e6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-03-13 20:03:47 -0700

    Fetch posts for paginated posts lists

--- pages/page/_id.vue
+++ pages/page/_id.vue
@@ -1,19 +1,38 @@
-
 <template>
   <section class="section">
     <div class="container">
-      <h1>
-        Index page number: {{ $route.params.id }}
+      <h1 class="title is-1 has-text-primary has-text-centered">
+        The Greatest Home Page Ever
       </h1>
-      <nuxt-link to="/">
-        Back Home Baby
-      </nuxt-link>
+      <h3 v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </h3>
     </div>
   </section>
 </template>
 
 <script>
+import { ghostAPI } from '@/util/ghost'
 export default {
-  name: 'PostList'
+  name: 'PostListPaginated',
+  async asyncData({ params, error, payload }) {
+    if (payload) {
+      return {
+        posts: payload,
+        pagination: payload.meta.pagination
+      }
+    } else {
+      // need to add catch if page doesn't exist at some point
+      const posts = await ghostAPI().posts.browse({
+        limit: process.env.POSTS_PER_PAGE,
+        page: params.id,
+        order: 'published_at DESC'
+      })
+      return {
+        posts,
+        pagination: posts.meta.pagination
+      }
+    }
+  }
 }
 </script>
