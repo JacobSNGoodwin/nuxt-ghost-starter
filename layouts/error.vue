417b0734115c1abe30ee9ac1383965d10f8f5f10
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-03-20 13:59:23 -0700

    Create a simple error layout

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404" class="title is-1 has-text-centered">
+      Page not found
+    </h1>
+    <h1 v-else class="title is-1 has-text-centered">
+      An error occurred
+    </h1>
+    <div class="buttons is-centered">
+      <nuxt-link to="/" tag="button" class="button is-info">
+        Home page
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    margin-top: 2em;
+    margin-bottom: 2em;
+  }
+</style>
