5e272613caa6fe270ac57fb06356478465447c66
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-03-21 10:00:05 -0700

    Add app icon, use pwa for favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,16 +14,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#e84b0d' },
 
   /*
   ** Global CSS
